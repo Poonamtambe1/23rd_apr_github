f1c80a13c66f347c5b5397a6fbcb7f0d8b062adc
Dell <Dell@DESKTOP-0L7J8AE>
2022-09-15 20:02:04 -0700

    [poonam]: second changes in project

---
+++ src/github/sample2.java
@@ -0,0 +1,13 @@
+package github;
+
+public class sample2
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hey Bhagwan suno hamari");
+		
+		System.out.println("Calls aane do");
+		
+	}
+
+}
