46b41a507478812f0b29e90fe5d9e58233e23b8a
Dell <Dell@DESKTOP-0L7J8AE>
2022-09-15 19:44:52 -0700

    [Poonam]: 1st changes in project

---
+++ src/github/sample.java
@@ -0,0 +1,13 @@
+package github;
+
+public class sample
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hiiiiii dear nikita");
+		
+		System.out.println("what's up babyyyy");
+		
+	}
+
+}
